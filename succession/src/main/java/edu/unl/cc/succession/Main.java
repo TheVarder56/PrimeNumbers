b0d924dec431535f0edf00edaa690552f3245046
TheVarder56 <derick.vargas@unl.edu.ec>
2025-06-16 18:07:25 -0500

    Update Main.java

--- succession/src/main/java/edu/unl/cc/succession/Main.java
+++ succession/src/main/java/edu/unl/cc/succession/Main.java
@@ -15,52 +15,67 @@ import java.util.Scanner;
     public class Main {
 
 
-        private static int readOptionsMenu(Scanner sc){
+       private static int readOptionsMenu(Scanner sc){
             int opcion;
-            System.out.println("Elija la opción de la serie que desea calcular:");
             System.out.println("Serie de primos elevados a impares hasta n térmimos");
             System.out.println("1. Ingrese la cantidad de terminos que desee imprimir ");
             System.out.println("2. Imprimir la serie");
+            System.out.println("0. Salir");
+
+            System.out.println("Elija una opcion:");
             opcion = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Choose an option:");
             return opcion;
         }
-
-
-
+        
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.printf("Programa de calculadora de sucesiones%n%n");
-            int opcionMenu = readOptionsMenu(sc);
-            System.out.println("Elija el límite/N términos  de la serie: ");
-            int limite = sc.nextInt();
+            //por defecto entra a ingresar datos
+            int opcionMenu=1;
             Successionable successionable = null;
+            do {
+                try {
+                    System.out.printf("Programa de calculadora de sucesiones%n%n");
+                    opcionMenu = readOptionsMenu(sc);
+                    successionable=switchOpciones(opcionMenu,sc,successionable);
+                } catch (Exception e) {
+                    System.out.println("Ingrese un numero valido");
+                    sc.nextLine();
+                }
+            }while(opcionMenu!=0);
+
+        }
+        private static Successionable switchOpciones(int opcionMenu,Scanner sc,Successionable successionable) {
             switch (opcionMenu){
                 case 1:
-                    successionable = new PrimeNumberCalculatorUpToNTerm(limite);
-                    System.out.println("Ingresado correctamente");
-                    break;
+                    try {
+                        System.out.println("Elija el límite/N términos  de la serie: ");
+                        int limite = sc.nextInt();
+                        successionable = new PrimeNumberCalculatorUpToNTerm(limite);
+                        System.out.println("Ingresado correctamente");
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        sc.nextLine();
+                    }finally {
+                        break;
+                    }
                 case 2:
                     System.out.println("La serie ingresada es:");
                     try{
                         Long result = successionable.calculate().longValue();
                         System.out.println(successionable.print());
+                        System.out.println("El resultado de la serie es: \nS = " + result);
                     }catch (Exception e){
                         System.out.println("Debe ingresar una opcion antes de imprimir la serie");
                     }
-
                     break;
+                    case 0:
+                        System.out.println("Gracias por Usar");
+                        System.out.println("Autores:\nMiguel Armas \nSoledad Buri \nSebastian Orellana \nSteven Perez  \nDerick Vargas");
                 default:
+
                     System.out.println("Opción no deseda");
             }
-            if (successionable != null){
-                Long result = successionable.calculate().longValue();
-                System.out.println(successionable.print());
-                System.out.println("S = " + result + "\n");
-            }
-
+            return successionable;
         }
-
     }
 
